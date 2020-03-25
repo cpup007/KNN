8e9823bc4ffa4b520a36130f85fb04326401dd2e
kokopop <38803432+cpup007@users.noreply.github.com>
2020-03-25 16:08:02 +0800

    Add files via upload

---
+++ model_selection.py
@@ -0,0 +1,28 @@
+#-*-coding:utf-8-*-
+#auth:kokopop
+from builtins import sum, len,int
+import numpy as np
+
+def train_test_spilt(X,y,test_ratio=0.2,seed=None):
+    """将数据X和y按照test_ratio分割成X_train,X_test,y_train,y_test"""
+    assert X.shape[0]==y.shape[0],\
+    "the size of X must be equal to the size of y "
+    assert 0.0 <=test_ratio<=1.0,\
+    "test_retion must be valid"
+
+    if seed:
+        np.random.seed(seed)
+
+    shuffle_indexes=np.random.permutation(len(X))
+
+    test_size=int(len(X)*test_ratio)
+    test_indexes=shuffle_indexes[:test_size]
+    train_indexes=shuffle_indexes[test_size:]
+
+    X_train=X[train_indexes]
+    y_train=y[train_indexes]
+
+    X_test=X[test_indexes]
+    y_test=y[test_indexes]
+
+    return X_train,y_train,X_test,y_test
